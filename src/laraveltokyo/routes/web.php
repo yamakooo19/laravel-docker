77ca602a6999275f0e3abffe3de2a0b028bf6150
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-30 18:10:06 +0900

    コントローラフォルダ内に分けられた各フォルダごとのコントローラをグループ化してルート設定する方法

--- src/laraveltokyo/routes/web.php
+++ src/laraveltokyo/routes/web.php
@@ -18,6 +18,18 @@ Route::get('/', function () {
 });
 
 Route::middleware([HelloMiddleware::class])->group(function () {
-    Route::get('/hello','HelloController@index');
-    Route::get('hello/other','HelloController@other');
+    Route::get('/hello', 'HelloController@index');
+    Route::get('hello/other', 'HelloController@other');
 });
+
+//下記のようにすることでControllersフォルダ内のControllerをグループとしてまとめて整理して設定できる
+Route::namespace('Sample')->group(function () {
+    Route::get('/sample', 'SampleController@index');
+    Route::get('/sample/other', 'SampleController@other');
+});
+
+//上記のようにしない場合は、こうなる
+//今回の例のように名前空間が、Controllers\Sample\XXXXXController.phpならまだ簡単だが、
+//Controllers\Sample\SampleA\SampleA1\XXXXXController.phpだと複雑化するので、上記のようにグループ化してまとめておいた方がいい。
+// Route::get('/sample', 'Sample\SampleController@index');
+// Route::get('/sample/other', 'Sample\SampleController@other');
