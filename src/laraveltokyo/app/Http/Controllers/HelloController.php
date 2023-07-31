518af71567e031e4e87cdf0ce765028b73958f60
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-07-31 22:38:11 +0900

    Storageクラスを使用したファイルの読み込みやファイルへの追記

--- src/laraveltokyo/app/Http/Controllers/HelloController.php
+++ src/laraveltokyo/app/Http/Controllers/HelloController.php
@@ -4,26 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Person;
+use Illuminate\Support\Facades\Storage;
 
 class HelloController extends Controller
 {
+
+    private $fname;
     function __construct()
     {
-        config(['sample.message' => '新しいメッセージ！']);
+        $this->fname = 'sample.txt';
     }
     public function index()
     {
-        $sample_msg = env('SAMPLE_MESSAGE');
-        $sample_data = env('SAMPLE_DATA');
+        $sample_msg = $this->fname;
+        $sample_data = Storage::get($this->fname);
         $data = [
             'msg' => $sample_msg,
-            'data' => explode(',', $sample_data),
+            'data' => explode(PHP_EOL, $sample_data)
         ];
         return view('hello.index', $data);
     }
 
-    public function other(Request $request)
+    public function other($msg)
     {
-        return redirect()->route('sample');
+        // $data = Storage::get($this->fname) . PHP_EOL . $msg;
+        // Storage::put($this->fname, $data);
+        // return redirect()->route('hello');
+
+        //上記と実装している処理は同じこと
+        //ファイルの末尾に追加するStorage::appendの他に
+        //ファイルの先頭に追加するStorage::prependがある。
+        Storage::append($this->fname, $msg);
+        return redirect()->route('hello');
     }
 }
--- src/laraveltokyo/routes/web.php
+++ src/laraveltokyo/routes/web.php
@@ -18,6 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', 'HelloController@index');
-Route::get('/hello/other', 'HelloController@other');
-Route::get('/sample', 'Sample\SampleController@index')->name('sample');
+Route::get('/hello', 'HelloController@index')->name('hello');
+Route::get('/hello/{msg}', 'HelloController@other');
