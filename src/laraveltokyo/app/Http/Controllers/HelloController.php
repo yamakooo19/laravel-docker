8c88beef0a933c03337ec462f5c50916dded5a4b
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-08-02 16:49:41 +0900

    ファサードを作成し、利用する

--- src/laraveltokyo/app/Http/Controllers/HelloController.php
+++ src/laraveltokyo/app/Http/Controllers/HelloController.php
@@ -6,22 +6,19 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Person;
 use Illuminate\Support\Facades\Storage;
-use App\MyClasses\MyService;
+
 use App\MyClasses\MyServiceInterface;
+use App\Facades\MyService;
 
 class HelloController extends Controller
 {
-    function __construct()
+    public function index(int $id = -1)
     {
-    }
-    public function index(MyServiceInterface $myservice, int $id = -1)
-    {
-        $myservice->setId($id);
+        MyService::setId($id);
         $data = [
-            'msg' => $myservice->say(),
-            'data' => $myservice->alldata(),
+            'msg' => MyService::say(),
+            'data' => MyService::alldata()
         ];
-
         return view('hello.index', $data);
 
     }
