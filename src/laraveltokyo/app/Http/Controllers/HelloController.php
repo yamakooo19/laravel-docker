ff3281f67e21863eec4e5b027a8323f2f907daa1
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-08-02 12:56:01 +0900

    コントローラのアクションメソッドで引数に指定せずにサービスコンテナのインスタンスを取得する

--- src/laraveltokyo/app/Http/Controllers/HelloController.php
+++ src/laraveltokyo/app/Http/Controllers/HelloController.php
@@ -10,8 +10,9 @@ use App\MyClasses\MyService;
 
 class HelloController extends Controller
 {
-    public function index(MyService $myservice)
+    public function index()
     {
+        $myservice = app('App\MyClasses\MyService');
         $data = [
             'msg' => $myservice->say(),
             'data' => $myservice->data(),
