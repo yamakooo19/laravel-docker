4d640287394ca30c4a8fa34577e3cf9743d29693
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-08-02 17:11:59 +0900

    ミドルウェアでのafter処理の追加

--- src/laraveltokyo/app/Http/Middleware/MyMiddleware.php
+++ src/laraveltokyo/app/Http/Middleware/MyMiddleware.php
@@ -25,6 +25,13 @@ class MyMiddleware
         ];
         $request->merge($merge_data);
 
-        return $next($request);
+        $response = $next($request);
+
+        $content = $response->content();
+        $content .= '<style> body {background-color:#eef; } p { font-size:18pt; } li { color: red; font-weight:bold; }</style>';
+
+        $response->setContent($content);
+
+        return $response;
     }
 }
