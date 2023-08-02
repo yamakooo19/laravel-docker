4ab2adb5d70da910190bef90fb11a87f621e8c41
Kosuke Yamaguchi <tshantian948@gmail.com>
2023-08-02 18:14:44 +0900

    whereメソッドによる検索条件

--- src/laraveltokyo/routes/web.php
+++ src/laraveltokyo/routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', 'HelloController@index')->middleware('MyMW');
+Route::get('/hello', 'HelloController@index');
 
-Route::get('/hello/{id}', 'HelloController@index')->middleware(App\Http\Middleware\MyMiddleware::class);
+Route::get('/hello/{id}', 'HelloController@index');
